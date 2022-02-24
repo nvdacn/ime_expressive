da0c3d0cf473462ef02ee647c72dabcc81d2e70d
cary-rowen <manchen_0528@outlook.com>
2022-02-24 20:02:54 +0800

    Improved application module support,

--- addon/appModules/sgtool.py
+++ addon/appModules/sgtool.py
@@ -3,23 +3,22 @@
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
-import appModuleHandler, speech, api, controlTypes, ui
+import appModuleHandler, speech, api
 from keyboardHandler import KeyboardInputGesture
-import controlTypes
 
 class AppModule(appModuleHandler.AppModule):
 
 	def script_pressKey(self,gesture):
 		gesture.send()
 		newObject=api.getFocusObject()
-		name=newObject.name if newObject.name else ""
-		role=controlTypes.roleLabels[newObject.role] if controlTypes.roleLabels[newObject.role] else ""
-		accDescription = newObject.description if newObject.description else ""
 		state=""
-		if newObject.role in(controlTypes.ROLE_CHECKBOX, controlTypes.ROLE_RADIOBUTTON):
-			state = "已选中" if controlTypes.STATE_CHECKED in newObject.states else "未选中"
-		ui.message(name +role +state+accDescription)
-#		speech.speakObject(newObject)
+		name=newObject.name if newObject.name else ""
+		role=newObject.role.displayString if newObject.role else ""
+		accDescription = newObject.description if newObject.description else ""
+		if newObject.role in(5, 6): # 5, 6 are equivalents of controlTypes.Role.CHECKBOX, controlTypes.Role.RADIOBUTTON
+			state = "已选中" if 32 in newObject.states else "未选中" # 32 is the equivalent of controlTypes.STATE_CHECKED
+		speech.speakMessage(name +role +state+accDescription)
+
 
 	__gestures = {
 "kb:tab":"pressKey",
